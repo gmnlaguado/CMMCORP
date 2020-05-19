9bbe4c029d008b3b4127c5ea15cd6114fa683617
Arnulfo R <drarn96@gmail.com>
2020-05-19 12:16:15 -0500

    Diagnostico Empresarial finished

--- windows/DiagnosticoEmpresarial.py
+++ windows/DiagnosticoEmpresarial.py
@@ -3,6 +3,8 @@ from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
 from declarations import querys
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.checkbox import CheckBox
 
 class DiagnosticoEmpresarialScreen(Screen):
     id_container_grid_1: ObjectProperty()
@@ -12,9 +14,12 @@ class DiagnosticoEmpresarialScreen(Screen):
 
         questions = querys.parametricList('businessDiagnosisQuestions')
         categories = querys.parametricList('businessDiagnosisCategories')
+        answers = querys.parametricList('businessDiagnosisAnswers')
         counter = 0
+        totalAnswers = 0
         self.id_container_grid_1.bind(minimum_height=self.id_container_grid_1.setter('height'))
         for idx, quest in enumerate(questions):
+            # Question Category
             if idx % 5 == 0 and counter < len(categories):
                 cat = Label(text=categories[counter], halign="center", valign="middle", size_hint=(None, None),
                         size=(853, 51), color=(0, 0, 0, 0.85), font_size=30, font_name="montserrat",
@@ -23,9 +28,26 @@ class DiagnosticoEmpresarialScreen(Screen):
                 counter += 1
                 self.id_container_grid_1.add_widget(cat)
 
+            # Questions Statement
+            box_container = BoxLayout(size_hint= (None, None), size =(1035, 110))
             lab = Label(text=quest, halign="center", valign="middle", size_hint=(None, None),
                         size=(294, 109), color=(0, 0, 0, 0.85), font_size=20, font_name="montserrat",
                         text_size=(294, 109))
-            #box_container = BoxLayout(size_hint= (None, None), size =(661, 108), spacing=12)
-            self.id_container_grid_1.add_widget(lab)
-            #self.id_container_grid_1.add_widget(box_container)
+            box_container.add_widget(lab)
+            
+            # Options to Answer with its checboxes
+            answers_container = BoxLayout(size_hint= (None, None), size =(696, 110), spacing=12, orientation="vertical")
+            for i in range(4):
+                # Single Option
+                line_container = BoxLayout(size_hint= (None, None), size =(696, 20))
+                ans = Label(text=answers[totalAnswers], halign="center", valign="middle", size_hint=(None, None),
+                        size=(661, 18), color=(0, 0, 0, 0.85), font_size=14, font_name="montserrat",
+                        text_size=(661, 18))
+                check = CheckBox(group=f"pregunta_{counter}", color=(0, 1, 0, 1))
+                totalAnswers += 1
+                line_container.add_widget(ans)
+                line_container.add_widget(check)
+                answers_container.add_widget(line_container) 
+            box_container.add_widget(answers_container)
+
+            self.id_container_grid_1.add_widget(box_container)
