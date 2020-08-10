5fe2ce1d3dec68310351d45e3a8f9f9e29aecbfc
drarn96 <drarn96@gmail.com>
2020-08-09 22:40:30 -0500

    Compilación de la APK y envío de peticiones Post

--- declarations/upload_process.py
+++ declarations/upload_process.py
@@ -1,14 +1,33 @@
 # coding=utf-8
-from declarations import class_declaration, querys
+from declarations import querys
+from kivy.network.urlrequest import UrlRequest
+import json
 
 
 def uploadInformation():
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    url = 'https://httpbin.org/post'
+    method = 'POST'
+
     tablas = querys.lista_de_tablas()
 
     for tab in tablas:
         res = querys.obtener_toda_info(tab)
         if len(res) > 0:
-            print(tab, res)
+            res = json.dumps(dict(zip(querys.bringColumns(tab), list(res))))
+            req = UrlRequest(url=url,
+            on_success=request_sucess,
+            on_progress=request_progress,
+            req_body=res,
+            req_headers=headers,
+            method=method)
 
-    for tabs in tablas:
-        querys.limpiar_tabla(tabs)
+    #for tabs in tablas:
+    #    querys.limpiar_tabla(tabs)
+
+
+def request_sucess(req, result):
+    print("\n\n\n Respuesta correcta \n\n\n")
+
+def request_progress(req, current_size, total_size):
+    print("\n\n\n Respuesta en proceso \n\n\n")
